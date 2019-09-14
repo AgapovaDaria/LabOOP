45f071c54970f27a6bbcafe634be708891943ecd
Agapova <agapovadaria2019@gmail.com>
2019-09-15 01:47:16 +0400

    Функци возведение в степень 3

---
+++ src/main/java/ru.ssau.tk.Lab2.LabOOP/functions/ThirdFunction.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.Lab2.LabOOP.functions;
+
+public class ThirdFunction implements MathFunction {
+
+    @Override
+    public double apply(double x) {
+        return java.lang.Math.pow(x,3);
+    }
+}
