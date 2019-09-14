aa63213c77f7867822d451c64964fade9a6f5c43
Alena Gerasimova <alena.gerasimova1999@gmail.com>
2019-09-14 15:25:11 +0400

    Commit 1Y

--- src/main/java/functions/MathFunction.java
+++ src/main/java/functions/MathFunction.java
@@ -1,7 +1,7 @@
 package functions;
 
-public interface MathFunction {
-    static double apply (double x) {
+interface MathFunction {
+    static double apply (double x){
         return x;
     }
 }
