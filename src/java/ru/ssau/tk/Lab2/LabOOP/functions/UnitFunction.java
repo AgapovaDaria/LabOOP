816aeac7ccb7a66b544647a7f917112cbdd5105e
Agapova <agapovadaria2019@gmail.com>
2019-09-15 01:10:50 +0400

    Изменение структуры

--- src/java/ru/ssau/tk/Lab2/LabOOP/functions/UnitFunction.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk.Lab2.LabOOP.functions;
-
-public class UnitFunction extends ConstantFunction{
-    public UnitFunction(){
-        super(1);
-    }
-}
--- src/java/ru/ssau/tk/Lab2/LabOOP/functions/ZeroFunction.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk.Lab2.LabOOP.functions;
-
-public class ZeroFunction extends ConstantFunction{
-    public ZeroFunction() {
-        super(0);
-    }
-}
