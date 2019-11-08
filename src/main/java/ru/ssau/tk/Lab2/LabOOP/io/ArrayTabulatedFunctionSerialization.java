7002d8c88e50b76fcfe10519fcced58372e7c1f0
alyonagerasimova <alena.gerasimova1999@gmail.com>
2019-11-08 20:55:42 +0400

    Task 3.6 by Y of lab3

--- src/main/java/ru/ssau/tk/Lab2/LabOOP/io/ArrayTabulatedFunctionSerialization.java
+++ src/main/java/ru/ssau/tk/Lab2/LabOOP/io/ArrayTabulatedFunctionSerialization.java
@@ -1,18 +1,35 @@
 package ru.ssau.tk.Lab2.LabOOP.io;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import operations.TabulatedDifferentialOperator;
+import ru.ssau.tk.Lab2.LabOOP.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.Lab2.LabOOP.functions.SqrFunction;
+import ru.ssau.tk.Lab2.LabOOP.functions.TabulatedFunction;
+import ru.ssau.tk.Lab2.LabOOP.functions.factory.ArrayTabulatedFunctionFactory;
+import java.io.*;
 
 public class ArrayTabulatedFunctionSerialization {
     public static void main(String[] args) {
-        File file = new File("output/serialized array functions.bin");
-        try(FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+        File file = new File("output/serialized_array_functions.bin");
+        TabulatedFunction function = new ArrayTabulatedFunction(new SqrFunction(), 3, 10, 50);
+        TabulatedDifferentialOperator differential = new TabulatedDifferentialOperator(new ArrayTabulatedFunctionFactory());
+        TabulatedFunction firstDifferential = differential.derive(function);
+        TabulatedFunction secondDifferential = differential.derive(firstDifferential);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+
             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
-        }
-        catch (IOException e){
+            FunctionsIO.serialize(bufferedOutputStream, function);
+            FunctionsIO.serialize(bufferedOutputStream, firstDifferential);
+            FunctionsIO.serialize(bufferedOutputStream, secondDifferential);
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try (FileInputStream inputStream = new FileInputStream(file);
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)) {
+                            System.out.println(FunctionsIO.deserialize(bufferedInputStream).toString() + "\n"
+                    + FunctionsIO.deserialize(bufferedInputStream).toString() + "\n" + FunctionsIO.deserialize(bufferedInputStream).toString());
+        } catch (IOException | ClassNotFoundException ic) {
+            ic.printStackTrace();
         }
     }
 }
