834962b62250deddac0ee100a5b8532cfe2242c6
Agapova <agapovadaria2019@gmail.com>
2019-11-08 13:51:45 +0400

    реализован класс TabulatedFunctionFileOutputStream

--- src/main/java/ru/ssau/tk/Lab2/LabOOP/io/TabulatedFunctionFileOutputStream.java
+++ src/main/java/ru/ssau/tk/Lab2/LabOOP/io/TabulatedFunctionFileOutputStream.java
@@ -1,4 +1,31 @@
 package ru.ssau.tk.Lab2.LabOOP.io;
 
+import ru.ssau.tk.Lab2.LabOOP.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.Lab2.LabOOP.functions.LinkedListTabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class TabulatedFunctionFileOutputStream {
+
+    public static void main(String[] args) {
+        double[] xValues = {5., 6., 7., 8.};
+        double[] yValues = {14., 10., 16., 9.};
+        ArrayTabulatedFunction arrayFun = new ArrayTabulatedFunction(xValues, yValues);
+        try {
+            BufferedOutputStream outStreamArray = new BufferedOutputStream(new FileOutputStream(new File("output/array function.bin")));
+            FunctionsIO.writeTabulatedFunction(outStreamArray, arrayFun);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LinkedListTabulatedFunction linkedListFun = new LinkedListTabulatedFunction(xValues, yValues);
+        try {
+            BufferedOutputStream outStreamLinkedList = new BufferedOutputStream(new FileOutputStream(new File("output/linked list function.bin")));
+            FunctionsIO.writeTabulatedFunction(outStreamLinkedList, linkedListFun);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
