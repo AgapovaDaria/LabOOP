35f0afe7c4b751ad8a7499ef8b7124e37fde59c5
Agapova <agapovadaria2019@gmail.com>
2019-11-16 23:11:54 +0400

    класс WriteTask

--- src/main/java/ru/ssau/tk/Lab2/LabOOP/concurrent/WriteTask.java
+++ src/main/java/ru/ssau/tk/Lab2/LabOOP/concurrent/WriteTask.java
@@ -2,17 +2,21 @@ package ru.ssau.tk.Lab2.LabOOP.concurrent;
 
 import ru.ssau.tk.Lab2.LabOOP.functions.TabulatedFunction;
 
-public class ReadTask implements Runnable {
-    private TabulatedFunction function;
+public class WriteTask implements Runnable {
 
-    ReadTask(TabulatedFunction function) {
+    private TabulatedFunction function;
+    private double value;
+
+    WriteTask(TabulatedFunction function, double value) {
         this.function = function;
+        this.value = value;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < function.getCount(); i++) {
-            System.out.printf("After read: i = %dx = %f, y = %f\n", i, function.getX(i), function.getY(i));
+            function.setY(i, value);
+            System.out.printf("Writing for index %d complete\n", i);
         }
     }
 }
