209a537e0fe48c69f5ebd162f8ae94a753914cf1
alyonagerasimova <alena.gerasimova1999@gmail.com>
2019-12-26 21:48:20 +0400

    Lab 3 TabulatedFunction createUnmodifiable() with tests

--- src/main/java/ru/ssau/tk/Lab2/LabOOP/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/Lab2/LabOOP/functions/factory/TabulatedFunctionFactory.java
@@ -1,17 +1,28 @@
 package ru.ssau.tk.Lab2.LabOOP.functions.factory;
 
-
 import ru.ssau.tk.Lab2.LabOOP.functions.MathFunction;
 import ru.ssau.tk.Lab2.LabOOP.functions.StrictTabulatedFunction;
 import ru.ssau.tk.Lab2.LabOOP.functions.TabulatedFunction;
+import ru.ssau.tk.Lab2.LabOOP.functions.UnmodifiableTabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
     TabulatedFunction create(MathFunction source, double xFrom, double xTo, int count);
+
     default TabulatedFunction createStrict(double[] xValues, double[] yValues) {
         return new StrictTabulatedFunction(create(xValues, yValues));
     }
+
     default TabulatedFunction createStrict(MathFunction source, double xFrom, double xTo, int count) {
-        return new StrictTabulatedFunction(create(source, xFrom,xTo,count));
+        return new StrictTabulatedFunction(create(source, xFrom, xTo, count));
+    }
+
+    default TabulatedFunction createUnmodifiable(double[] xValues, double[] yValues) {
+        return new UnmodifiableTabulatedFunction(create(xValues, yValues));
+    }
+
+    default TabulatedFunction createUnmodifiable(MathFunction source, double xFrom, double xTo, int count) {
+        return new UnmodifiableTabulatedFunction(create(source, xFrom, xTo, count));
     }
 }
+
--- src/test/java/ru/ssau/tk/Lab2/LabOOP/functions/factory/ArrayTabulatedFunctionFactoryTest.java
+++ src/test/java/ru/ssau/tk/Lab2/LabOOP/functions/factory/ArrayTabulatedFunctionFactoryTest.java
@@ -6,10 +6,12 @@ import org.testng.annotations.Test;
 import ru.ssau.tk.Lab2.LabOOP.functions.ArrayTabulatedFunction;
 import ru.ssau.tk.Lab2.LabOOP.functions.StrictTabulatedFunction;
 import ru.ssau.tk.Lab2.LabOOP.functions.TabulatedFunction;
+import ru.ssau.tk.Lab2.LabOOP.functions.UnmodifiableTabulatedFunction;
 
 public class ArrayTabulatedFunctionFactoryTest {
     private final double[] xValues = new double[]{1., 3., 4., 8., 12.};
     private final double[] yValues = new double[]{8., 6., 5., 17., 16.};
+    private TabulatedFunction function = (new ArrayTabulatedFunctionFactory().createUnmodifiable(xValues, yValues));
 
     @Test
     public void testCreate() {
@@ -34,5 +36,60 @@ public class ArrayTabulatedFunctionFactoryTest {
         assertEquals(array.iterator().next().x, 1);
         assertEquals(array.apply(3), 6);
         assertThrows(UnsupportedOperationException.class, () -> array.apply(2.5));
+
+    }
+    @Test
+    public void testCreateUnmodifiable() {
+        assertTrue(function instanceof UnmodifiableTabulatedFunction);
+    }
+
+    @Test
+    public void testGetCount() {
+        assertEquals(function.getCount(),5);
+    }
+
+    @Test
+    public void testGetX() {
+        assertEquals(function.getX(3), 8.);
+    }
+
+    @Test
+    public void testGetY() {
+        assertEquals(function.getY(1), 6.);
+    }
+
+    @Test
+    public void testSetY() {
+        assertThrows(UnsupportedOperationException.class, () -> function.setY(2, 1.));
+    }
+
+    @Test
+    public void testApply() {
+        assertEquals(function.apply(3), 6);
+    }
+
+    @Test
+    public void testLeftBound() {
+        assertEquals(function.leftBound(), 1.);
+    }
+
+    @Test
+    public void testRightBound() {
+        assertEquals(function.rightBound(), 12.);
+    }
+
+    @Test
+    public void testIndexOfX() {
+        assertEquals(function.indexOfX(1.), 0);
+    }
+
+    @Test
+    public void testIndexOfY() {
+        assertEquals(function.indexOfY(16.), 4);
+    }
+
+    @Test
+    public void testIterator() {
+        assertEquals(function.iterator().next().x, 1);
     }
 }
